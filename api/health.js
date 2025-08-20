07a5d238152dcd94db379553a3662da8b63bc304
roohianr <109872824+roohianr@users.noreply.github.com>
2025-08-20 23:50:33 +0200

    Delete api/health.js

--- api/health.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    hasUrl: !!process.env.SUPABASE_URL,
-    hasKey: !!process.env.SUPABASE_SERVICE_KEY,
-    node: process.version
-  });
-}
